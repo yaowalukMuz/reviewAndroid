fb9603a48e685f8c636dca2937251f2c7bb3aa7b
yaowamuz <yaowamuz.c@gmail.com>
2017-07-24 13:51:21 +0700

    start

---
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/reviewandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hitachi_tstv.yodpanom.yaowaluk.reviewandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
